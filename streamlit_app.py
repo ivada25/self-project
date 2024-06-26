f5c4d88d7170f3b5f426cf10a6d69bef5858a2f6
ivada25 <161428694+ivada25@users.noreply.github.com>
2024-06-27 00:24:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import train_test_split
 # from imblearn.over_sampling import SMOTE
 
 st.set_page_config(page_title="Pendat", layout="wide")
-navbar()
 
 # Daftar fitur
 fitur = ['fLength', 'fWidth', 'fSize', 'fConc', 'fConc1', 'fAsym', 'fM3Long', 'fM3Trans', 'fAlpha', 'fDist']
